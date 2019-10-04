36eb8bb87a32514d8c2cb4d38c9a7b413f121a20
Travis CI User <travis@example.org>
2019-10-04 10:01:37 +0000

    Updates

---
+++ precache-manifest.4bae694d01675cb8eaeb4a0f7a83b8d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91fddcb976b9062e49a5833e5c5c2eae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "195607bc602cf503cce3",
+    "url": "/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "50a125f30103ac15d4cd",
+    "url": "/static/js/2.1b1c9bd6.chunk.js"
+  },
+  {
+    "revision": "195607bc602cf503cce3",
+    "url": "/static/js/main.6d41b5c7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.080fa1a82942faedbc9f4ca1e48299db.js"
+  "/precache-manifest.4bae694d01675cb8eaeb4a0f7a83b8d6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
