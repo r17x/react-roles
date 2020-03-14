b7016e594321a15576beb51fb75880a810e85e9f
Travis CI User <travis@example.org>
2020-03-14 12:25:12 +0000

    Updates

---
+++ precache-manifest.1a73ce8bc2305227f8bcc685e06165af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ebd359b87bb2df91b881273fa683622",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48533a90e159bcafe063",
+    "url": "/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "1ff1c7d57ac27dc60715",
+    "url": "/static/js/2.e014234c.chunk.js"
+  },
+  {
+    "revision": "48533a90e159bcafe063",
+    "url": "/static/js/main.2d2b575e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
